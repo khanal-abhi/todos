7cafa4b5b26b476479c2b4e7bba0686cd1cb203f
Abhi Khanal <khanal.abhi@gmail.com>
2016-02-10 16:08:06 -0500

    Updated it.

--- app/src/main/java/co/khanal/todos/ListOfTodos.java
+++ app/src/main/java/co/khanal/todos/ListOfTodos.java
@@ -1,13 +1,27 @@
 package co.khanal.todos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListOfTodos extends AppCompatActivity {
+
+    Button ToAddATodo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_todos);
+
+        ToAddATodo = (Button) findViewById(R.id.ToAddAToDo);
     }
+
+    public View.OnClickListener ToAddAToDoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+        }
+    };
 }
